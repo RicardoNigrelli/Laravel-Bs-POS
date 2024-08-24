d7f9fdf83fc05c8b7567389249d558c1a94d7205
Ricardo Nigrelli <ricanigrelli996@gmail.com>
2024-08-24 12:20:11 -0300

    avance con base de datos

---
+++ laravel-bs-pos/app/Models/Categoría.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Categoría extends Model
+{
+    use HasFactory;
+}
